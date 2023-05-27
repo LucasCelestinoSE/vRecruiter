68cc5f1fb7cb37e01fe79a3d14c3bd515c03441d
Lucas Celestino <lucas.celp1@gmail.com>
2023-05-27 14:06:37 -0300

    adicionando variaveis de ambiente

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,14 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 
-const PORT = process.env.MYSQLPORT as number | undefined
+const PORT = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
 	type: 'mysql',
-	host: 'localhost',
-	port: 3306,
-	username: 'root',
-	password: 'lucas@98623457',
+	host: process.env.DB_HOST,
+	port: PORT,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: 'rr7',
 	entities: [`${__dirname}/**/entities/*.{ts,js}`],
 	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
